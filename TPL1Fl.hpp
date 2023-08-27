faf6c1c789723fc17858168aba2c518c9864369c
Oleh Kulykov <oleh.kulykov@stock3.com>
2023-08-27 11:43:57 +0200

    1693129437

--- TPL1Fl.hpp
+++ TPL1Fl.hpp
@@ -8,15 +8,27 @@
 template<typename T>
 struct TPL1Fl {
 public:
-    std::vector<uint8_t> vectorize(const Bytef * inBuff, const T inSize, const T outSize) {
-        std::vector<uint8_t> v;
+#if defined(__R2D9_RAW_HEAP_MEMORY_HPP__) && (__R2D9_RAW_HEAP_MEMORY_HPP__ > 0)
+    r2d9::RawHeapMemory memorize(const Bytef * inBuff, const T inSize, const T outSize) {
+        r2d9::RawHeapMemory v;
         v.resize(outSize);
         uLong destLen = outSize;
-        if (uncompress(v.data(), &destLen, inBuff, inSize) != Z_OK) {
+        if ((uncompress(static_cast<Bytef *>(v), &destLen, inBuff, inSize) != Z_OK) || (destLen != outSize)) {
             throw std::runtime_error("");
         }
         return v;
     }
+#else
+    std::vector<uint8_t> vectorize(const Bytef * inBuff, const T inSize, const T outSize) {
+        std::vector<uint8_t> v;
+        v.resize(outSize);
+        uLong destLen = outSize;
+        if ((uncompress(v.data(), &destLen, inBuff, inSize) != Z_OK) || (destLen != outSize)) {
+            throw std::runtime_error("");
+        }
+        return v;
+    }
+#endif
 };
 
 #endif
--- TPL2Fl.hpp
+++ TPL2Fl.hpp
@@ -10,13 +10,14 @@ struct TPL2Fl {
 private:
     ZSTD_DCtx * _dctx;
 public:
-    std::vector<uint8_t> vectorize(const void * inBuff, const T inSize, const T outSize) {
-        std::vector<uint8_t> v;
+#if defined(__R2D9_RAW_HEAP_MEMORY_HPP__) && (__R2D9_RAW_HEAP_MEMORY_HPP__ > 0)
+    r2d9::RawHeapMemory memorize(const void * inBuff, const T inSize, const T outSize) {
+        r2d9::RawHeapMemory v;
         const size_t dstCapacity = ZSTD_getDecompressedSize(inBuff, inSize);
         if (dstCapacity > 0 && !ZSTD_isError(dstCapacity)) {
             v.resize(dstCapacity);
-            const size_t dstSize = ZSTD_decompressDCtx(_dctx, v.data(), dstCapacity, inBuff, inSize);
-            if (ZSTD_isError(dstSize) || dstSize != outSize) {
+            const size_t dstSize = ZSTD_decompressDCtx(_dctx, static_cast<void *>(v), dstCapacity, inBuff, inSize);
+            if (ZSTD_isError(dstSize) || (dstSize != outSize)) {
                 throw std::runtime_error("");
             }
             if (v.size() != dstSize) {
@@ -25,6 +26,23 @@ public:
         }
         return v;
     }
+#else
+    std::vector<uint8_t> vectorize(const void * inBuff, const T inSize, const T outSize) {
+        std::vector<uint8_t> v;
+        const size_t dstCapacity = ZSTD_getDecompressedSize(inBuff, inSize);
+        if (dstCapacity > 0 && !ZSTD_isError(dstCapacity)) {
+            v.resize(dstCapacity);
+            const size_t dstSize = ZSTD_decompressDCtx(_dctx, v.data(), dstCapacity, inBuff, inSize);
+            if (ZSTD_isError(dstSize) || (dstSize != outSize)) {
+                throw std::runtime_error("");
+            }
+            if (v.size() != dstSize) {
+                v.resize(dstSize);
+            }
+        }
+        return v;
+    }
+#endif
     
     TPL2Fl() {
         _dctx = ZSTD_createDCtx();
